c8612f16624b7f4af55d3f7f2b57dca3816de357
zeyang1609 <zeyang.ho@gmail.com>
2026-08-28 07:57:31 +0800

    chore: remove redundant test_json.cs file

--- test_json.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Text.Json;
-public class Test { public string ChatID { get; set; } = "123"; }
-class Program { static void Main() { 
-    var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-    Console.WriteLine(JsonSerializer.Serialize(new Test(), options));
-} }
